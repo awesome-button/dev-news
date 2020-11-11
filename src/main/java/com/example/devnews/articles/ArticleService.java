fcd514a23ab597ed9204cd66db08a72174106724
awesome-button <oleshko.lena@gmail.com>
2020-11-11 16:56:27 +0100

    set up JPI repository and rewrote crud methods to use the repository instead of List

--- src/main/java/com/example/devnews/articles/ArticleService.java
+++ src/main/java/com/example/devnews/articles/ArticleService.java
@@ -1,54 +1,42 @@
 package com.example.devnews.articles;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
+@Service
 public class ArticleService {
+    @Autowired
+    ArticleRepo repository;
+//    List<Article> articles;
 
-    List<Article> articles;
-
-    public ArticleService() {
-        articles = new ArrayList<>();
-        articles.add(new Article("Google Play Store new feature", "Google’s latest Play Store feature helps users compare apps", "Ryan Daws", getId()));
-        articles.add(new Article("code overload", "51% of devs say they’re dealing with 100x more code than 10 years ago\n", "Ryan Daws", getId()));
-    }
+//    public ArticleService() {
+//        articles = new ArrayList<>();
+//        articles.add(new Article("Google Play Store new feature", "Google’s latest Play Store feature helps users compare apps", "Ryan Daws"));
+//        articles.add(new Article("code overload", "51% of devs say they’re dealing with 100x more code than 10 years ago\n", "Ryan Daws"));
+//    }
 
     public List<Article> getAll() {
-        return articles;
+        return repository.findAll();
     }
 
-    public Article getById(Long id) {
-        for (Article article : articles) {
-            if (article.getId().equals((id))) return article;
-        }
-        return null;
+    public Optional<Article> getById(Long id) {
+        return repository.findById(id);
     }
 
     public Article create(Article article) {
-        articles.add(article);
-        return article;
+        return repository.save(article);
     }
 
     public void delete(Long id) {
-        articles = articles.stream()
-                .filter(article -> !article.getId().equals(id))
-                .collect(Collectors.toList());
+        repository.deleteById(id);
     }
 
-    public Article update(Long id, Article updArticle) {
-        articles.forEach(article -> {
-            if (article.getId().equals(id)) {
-                articles.set(articles.indexOf(article), updArticle);
-            }
-        });
-        return null;
+    public Article update(Article updArticle) {
+        return repository.save(updArticle);
     }
 
-    private Long getId() {
-        return Long.valueOf(this.articles.size()+1);
-    }
 
 }
